73b9dcaf6d70eafacda409804501d14aedc6d28a
dilansamugutierrez <dilansamugutierrez@gmail.com>
2025-08-27 22:56:28 -0500

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { CartProvider } from "@/contexts/cart-context"
 import { AuthProvider } from "@/contexts/auth-context"
 
 export const metadata: Metadata = {
-  title: "v0 App",
+  title: "Tienda Online",
   description: "Created with v0",
   generator: "v0.app",
 }
